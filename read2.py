24e64252f1b93aa298ca1da7eb8c903154ee9b3d
rajataneja101 <tanejarajat101@gmail.com>
2017-06-05 14:25:44 +0530

    added UI using tkinter

--- read2.py
+++ read2.py
@@ -1,12 +1,31 @@
-import PyPDF2
-pr=PyPDF2.PdfFileReader('input.pdf','rb')
-dict=pr.getFields()
-print(dict)
-dict['Given Name Text Box']='rajat'
-print(dict)
-d=pr.getPage(0)
-outputstream = open('input.pdf', 'wb+')
-pw=PyPDF2.PdfFileWriter()
-pw.updatePageFormFieldValues(d,dict['Given Name Text Box'])
-pw.write(outputstream)
-outputstream.close()
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+global a
+def browse():
+    a.set(askopenfilename(title='select new file'))
+def browse_json():
+    x.set(askopenfilename(title='select new file'))
+def done():
+    print(e1.get())
+    print(e.get())
+root = Tk()
+
+a = StringVar()
+x= StringVar()
+l = Label(root, text="new file: ")
+l.pack()
+
+e = Entry(root, width=25, textvariable=a)
+e.pack()
+
+b = Button(root, text="Browse", command=browse)
+b.pack()
+l1=Label(root,text="new file:")
+l1.pack()
+e1 = Entry(root, width=25, textvariable=x)
+e1.pack()
+b1 = Button(root, text="Browse", command=browse_json)
+b1.pack()
+b2 = Button(root, text="OK!", command=done)
+b2.pack()
+root.mainloop()
--- read_pdf.py
+++ read_pdf.py
@@ -2,24 +2,78 @@ import PyPDF2
 import os
 from fdfgen import forge_fdf
 import json
-json_data = json.loads(open('customer.json').read())
-python_obj = json_data
-r2=python_obj
-print(python_obj['address']['streetAddress'])
-print(r2)
-pr=PyPDF2.PdfFileReader('input2.pdf','rb')
-dict=pr.getFields()
-print(dict)
-r1=list(dict.keys())
-i=0
-print(r1)
-for r in r1:
-    print(str(i)+" "+r)
-    i+=1
-fields = [(r1[0],r2['lastName']),(r1[1],r2['firstName']),(r1[2],r2['date']),(r1[4],'Yes'),(r1[3],'Yes')]
-fdf = forge_fdf("",fields,[],[],[],[])
-fdf_file = open("data.fdf","wb")
-fdf_file.write(fdf)
-fdf_file.close()
-os.system('pdftk "input2.pdf" fill_form "data.fdf" output "output.pdf" flatten')
-print("done")
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+global a,x,j_file,pdf_file
+def browse():
+    a.set(askopenfilename(title='select PDF file'))
+def browse_json():
+    x.set(askopenfilename(title='select JSON file'))
+def done():
+    pdf_file=e.get()
+    j_file=e1.get()
+    json_data = json.loads(open(j_file).read())
+    python_obj = json_data
+    r2 = python_obj
+    print(python_obj['address']['streetAddress'])
+    print(r2)
+    pr = PyPDF2.PdfFileReader(open(pdf_file, 'rb'), strict=False)
+    dict = pr.getFields()
+    print(dict)
+    r1 = list(dict.keys())
+    i = 0
+    print(r1)
+    for r in r1:
+        print(str(i) + " " + r)
+        i += 1
+    fields = [(r1[0], r2['lastName']), (r1[1], r2['firstName']), (r1[2], r2['date']), (r1[4], 'Yes'), (r1[5], 'Yes'),
+              (r1[3], 'Yes')]
+    fdf = forge_fdf("", fields, [], [], [], [])
+    fdf_file = open("data.fdf", "wb")
+    fdf_file.write(fdf)
+    fdf_file.close()
+    os.system('pdftk "input2.pdf" fill_form "data.fdf" output "output.pdf" flatten')
+    print("done")
+    json_data = json.loads(open(j_file).read())
+    python_obj = json_data
+    r2 = python_obj
+    print(python_obj['address']['streetAddress'])
+    print(r2)
+    pr = PyPDF2.PdfFileReader(open(pdf_file, 'rb'), strict=False)
+    dict = pr.getFields()
+    print(dict)
+    r1 = list(dict.keys())
+    i = 0
+    print(r1)
+    for r in r1:
+        print(str(i) + " " + r)
+        i += 1
+    fields = [(r1[0], r2['lastName']), (r1[1], r2['firstName']), (r1[2], r2['date']), (r1[4], 'Yes'), (r1[5], 'Yes'),
+              (r1[3], 'Yes')]
+    fdf = forge_fdf("", fields, [], [], [], [])
+    fdf_file = open("data.fdf", "wb")
+    fdf_file.write(fdf)
+    fdf_file.close()
+    os.system('pdftk "' + pdf_file + '" fill_form "data.fdf" output "output.pdf" flatten')
+    print("done")
+    exit()
+root = Tk()
+a = StringVar()
+x = StringVar()
+l = Label(root, text="pdf file: ")
+l.pack()
+
+e = Entry(root, width=25, textvariable=a)
+e.pack()
+
+b = Button(root, text="Browse", command=browse)
+b.pack()
+l1=Label(root,text="json file:")
+l1.pack()
+e1 = Entry(root, width=25, textvariable=x)
+e1.pack()
+b1 = Button(root, text="Browse", command=browse_json)
+b1.pack()
+b2 = Button(root, text="OK!", command=done)
+b2.pack()
+root.mainloop()
